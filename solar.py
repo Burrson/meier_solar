45abd79eeb6d938254b1175c24a7d7ea87896481
^_^ <32826270+hcm444@users.noreply.github.com>
2023-09-03 14:35:12 -0700

    Update solar.py
    Dual and Single Modes

--- solar.py
+++ solar.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import datetime
 import ephem
 import math
@@ -6,45 +7,43 @@ from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
 import pytz
 
-# Create a geocoder
 geolocator = Nominatim(user_agent="solar_panel_positioning")
-
-# Create a timezone finder
 timezone_finder = TimezoneFinder()
 
-# Prompt the user for a city or location
 location_name = input("Enter a city or location: ")
 
-# Use geocoding to obtain coordinates for the location
 location = geolocator.geocode(location_name)
 
 if location:
     latitude = location.latitude
     longitude = location.longitude
 
-    # Initialize Pygame
     pygame.init()
 
-    # Constants for Pygame display
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 600
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     FONT_SIZE = 20
+    BUTTON_WIDTH = 100
+    BUTTON_HEIGHT = 40
+    BUTTON_MARGIN = 10
+    BUTTON_COLOR = (0, 128, 255)
+    BUTTON_TEXT_COLOR = (255, 255, 255)
 
-    # Create a Pygame window
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Solar Panel Positioning")
+    current_mode = 'dual'
 
-    # Pygame font for displaying text
-    font = pygame.font.Font(None, FONT_SIZE)
+    def switch_mode():
+        global current_mode
+        if current_mode == 'dual':
+            current_mode = 'single'
+        else:
+            current_mode = 'dual'
 
-    # Function to draw the 3D solar panel
     def draw_solar_panel(pan_angle, tilt_angle):
         panel_length = 100
         panel_width = 100
 
-        # Calculate the corner points of the 3D panel
         top_left = [
             SCREEN_WIDTH // 2 - panel_length / 2,
             SCREEN_HEIGHT // 2 - panel_width / 2,
@@ -62,7 +61,6 @@ if location:
             SCREEN_HEIGHT // 2 + panel_width / 2,
         ]
 
-        # Rotate the panel based on pan_angle (around its center)
         center_x = (top_left[0] + top_right[0]) / 2
         center_y = (top_left[1] + bottom_left[1]) / 2
         angle_rad = math.radians(pan_angle)
@@ -99,7 +97,6 @@ if location:
             + (bottom_right[1] - center_y) * math.cos(angle_rad),
         ]
 
-        # Calculate the coordinates after tilting based on tilt_angle
         tilt_angle_rad = math.radians(tilt_angle)
         top_left_tilted = [
             top_left_rot[0],
@@ -122,61 +119,55 @@ if location:
             + (bottom_right_rot[1] - center_y) * math.cos(tilt_angle_rad),
         ]
 
-        # Draw the 3D panel as a filled polygon
         pygame.draw.polygon(
             screen,
             BLACK,
             [top_left_tilted, top_right_tilted, bottom_right_tilted, bottom_left_tilted],
         )
 
-    # Main game loop
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Solar Panel Positioning")
+    font = pygame.font.Font(None, FONT_SIZE)
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if BUTTON_RECT.collidepoint(event.pos):
+                    switch_mode()
 
-        # Create an observer for the specified location
         observer = ephem.Observer()
         observer.lat = str(latitude)
         observer.lon = str(longitude)
 
-        # Determine the timezone for the location
         timezone_str = timezone_finder.timezone_at(lng=longitude, lat=latitude)
 
         if timezone_str:
-            # Use the determined timezone
             local_timezone = pytz.timezone(timezone_str)
-
-            # Get the current time in the local timezone
             current_time = datetime.datetime.now(local_timezone)
         else:
-            # Fallback to UTC if timezone cannot be determined
             local_timezone = pytz.utc
             current_time = datetime.datetime.now(pytz.utc)
 
-        # Set the observer's date and time
         observer.date = current_time
 
         sun = ephem.Sun(observer)
-
-        # alt — Altitude ±90° relative to the horizon’s great circle (unaffected by the rise/set setting horizon)
         solar_altitude = math.degrees(sun.alt)
-
-        # az — Azimuth 0°–360° east of north
         solar_azimuth = math.degrees(sun.az)
 
-        optimal_pan_angle = solar_azimuth
+        if current_mode == 'dual':
+            optimal_pan_angle = solar_azimuth
+            optimal_tilt_angle = 90 - solar_altitude
+        else:
+            # Single-axis mode (horizontal tracking)
+            optimal_pan_angle = solar_azimuth
+            optimal_tilt_angle = 0  # Panel lies flat
 
-        optimal_tilt_angle = 90 - solar_altitude
-
-        # Clear the screen
         screen.fill(WHITE)
-
-        # Draw the 3D solar panel
         draw_solar_panel(optimal_pan_angle, optimal_tilt_angle)
 
-        # Print the results on the screen
         location_text = font.render(f"Location: {location_name}", True, BLACK)
         time_text = font.render(
             f"Current Time (local timezone): {current_time.strftime('%Y-%m-%d %H:%M:%S %Z')}",
@@ -203,9 +194,22 @@ if location:
         screen.blit(pan_angle_text, (10, 90))
         screen.blit(tilt_angle_text, (10, 110))
 
+        BUTTON_RECT = pygame.Rect(
+            SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2,
+            SCREEN_HEIGHT - BUTTON_HEIGHT - BUTTON_MARGIN,
+            BUTTON_WIDTH,
+            BUTTON_HEIGHT,
+        )
+
+        pygame.draw.rect(screen, BUTTON_COLOR, BUTTON_RECT)
+        button_font = pygame.font.Font(None, 36)
+        button_text = button_font.render(
+            f'Switch to {current_mode.capitalize()} Axis', True, BUTTON_TEXT_COLOR
+        )
+        screen.blit(button_text, BUTTON_RECT.move(10, 5))
+
         pygame.display.flip()
 
-    # Quit Pygame
     pygame.quit()
 else:
     print("Location not found.")
